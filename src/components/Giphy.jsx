3301865a640d90d130c48421c604ce2b1fa6d155
mattomolewa <matthew.omolewa@gmail.com>
2021-02-24 06:43:10 +0100

    api request

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -7,19 +6,25 @@ const Giphy = () => {
 
   async function getGifs() {
     try {
-      axios.get("api.giphy.com/v1/gifs/trending", {
+      const result = await axios.get("https://api.giphy.com/v1/gifs/trending", {
         params: {
           api_key: "V43NFKCMq3I1DObR3NbMXyUOj2Qe8mBU",
         },
       });
+      setGifs(result.data.data);
     } catch (error) {
       console.error(error.message);
     }
   }
 
+  useEffect(() => {
+    getGifs();
+    console.log(gifs);
+  }, []);
+
   return (
     <div>
-      <span></span>
+      <span>hello wro</span>
     </div>
   );
 };
