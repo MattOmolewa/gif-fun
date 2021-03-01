29312a82d663f0dd27b7a0aaf64ef7872c2fb049
mattomolewa <matthew.omolewa@gmail.com>
2021-03-01 11:05:59 +0100

    complete pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -15,12 +15,21 @@ const Pagination = ({
   return (
     <nav>
       <ul className="pagination d-flex justify-content-end">
-        {pageNumber.map((num, i) => (
-          <li key={i} className="page-item">
-            <a onClick={() => pageSelected(num)} className="page-link" href="#">
-              {num}
-            </a>
-          </li>
+        {pageNumber.map((num, i) => {
+          let classes = "page-item ";
+          if (num == currentPage) {
+            classes += "active";
+          }
+          return (
+            <li key={i} className={classes}>
+              <a
+                onClick={() => pageSelected(num)}
+                className="page-link"
+                href="#"
+              >
+                {num}
+              </a>
+            </li>
         ))}
       </ul>
     </nav>
